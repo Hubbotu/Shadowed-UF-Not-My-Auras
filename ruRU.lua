6ac2b32dcdf7cad5320d36d65e8340cd59544730
ZamestoTV <34053496+Hubbotu@users.noreply.github.com>
2025-03-02 16:34:32 +0300

    Add files via upload

---
+++ ruRU.lua
@@ -0,0 +1,37 @@
+-- Translator ZamestoTV -- ruRU
+local ADDON_NAME, PRIVATE_TABLE = ...
+local L = {}
+-- Строки для настроек затухания
+L["FadeBuffTitle"] = "Затухание"
+L["FadeBuffDesc"] = "Затухание баффов, которые не были наложены вами."
+L["FadeDebuffTitle"] = "Затухание"
+L["FadeDebuffDesc"] = "Затухание дебаффов, которые не были наложены вами."
+L["FadeOpacBuffTitle"] = "Прозрачность затухания"
+L["FadeOpacBuffDesc"] = "Насколько прозрачными будут баффы."
+L["FadeOpacDebuffTitle"] = "Прозрачность затухания"
+L["FadeOpacDebuffDesc"] = "Насколько прозрачными будут дебаффы."
+-- Строки для настроек затемнения
+L["DarkBuffTitle"] = "Затемнение"
+L["DarkBuffDesc"] = "Затемнение баффов, которые не были наложены вами."
+L["DarkDebuffTitle"] = "Затемнение"
+L["DarkDebuffDesc"] = "Затемнение дебаффов, которые не были наложены вами."
+L["DarkAmountBuffTitle"] = "Степень затемнения"
+L["DarkAmountBuffDesc"] = "Насколько темными будут баффы."
+L["DarkAmountDebuffTitle"] = "Степень затемнения"
+L["DarkAmountDebuffDesc"] = "Насколько темными будут дебаффы."
+-- Строки для настроек обесцвечивания
+L["DesatBuffTitle"] = "Обесцвечивание"
+L["DesatBuffDesc"] = "Обесцвечивание баффов, которые не были наложены вами."
+L["DesatDebuffTitle"] = "Обесцвечивание"
+L["DesatDebuffDesc"] = "Обесцвечивание дебаффов, которые не были наложены вами."
+-- Строки для настроек игнорирования
+L["IgnoreBossBuffTitle"] = "Игнорировать баффы боссов"
+L["IgnoreBossBuffDesc"] = "Не применять эти эффекты к баффам, наложенным боссами."
+L["IgnoreBossDebuffTitle"] = "Игнорировать дебаффы боссов"
+L["IgnoreBossDebuffDesc"] = "Не применять эти эффекты к дебаффам, наложенным боссами."
+L["IgnoreRemBuffTitle"] = "Игнорировать снимаемые баффы"
+L["IgnoreRemBuffDesc"] = "Не применять эти эффекты к баффам, которые можно снять."
+L["IgnoreRemDebuffTitle"] = "Игнорировать излечимые дебаффы"
+L["IgnoreRemDebuffDesc"] = "Не применять эти эффекты к дебаффам, которые можно излечить."
+
+PRIVATE_TABLE.L = L
